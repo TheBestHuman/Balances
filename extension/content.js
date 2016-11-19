b3f1052c8bc723621f7323d5612d5b06524fd06c
Brian Kimmel <kimmelb@gmail.com>
2016-11-18 20:00:43 -0500

    fixed some formatting problems by not performing the replacement unless the search string is present

--- extension/content.js
+++ extension/content.js
@@ -3,12 +3,14 @@ var URIs = [
                 , "https://www.plannedparenthood.org/donate"
                 , "http://www.adl.org/donate"
                 , "http://www.borderangels.org/our-causes/"
-                , "https://staywoke.org"
+                , "http://staywoke.org"
                 , "https://donate.bgca.org/site/Donation2?7942.donation=form1&df_id=7942&_ga=1.66097390.119300056.1478845330"
                 , "https://secure3.convio.net/cfrr/site/Donation2;jsessionid=97A4C7A9488B5DDF3450546DE1123DB2.app347a?1380.donation=form1&df_id=1380&web&_ga=1.145398384.513337705.1476995534"
                 , "https://www.splcenter.org/donate"
             ]
 
+//var searchString = /(trump)/gi;
+var searchString = /(trump)/gi;
 
 var replaceTextInNode = function(parentNode){
     for(var i = parentNode.childNodes.length-1; i >= 0; i--){
@@ -16,11 +18,28 @@ var replaceTextInNode = function(parentNode){
 
         //  Make sure this is a text node
         if(node.nodeType == Element.TEXT_NODE){
-            var newNode = document.createElement("span");
-            var URI = URIs[Math.floor(Math.random()*URIs.length)];
-            newNode.innerHTML = node.data.replace(/(trump)/gi, "<a style='text-decoration:inherit;font:inherit;display:inherit' href='" +  URI + "'>$1</a>");
+            var needsReplacement = (node.data.search(searchString) >= 0);
+            //needsReplacement = true;
+            if(needsReplacement)
+            {
+                var newNode = document.createElement("fake");
+                var URI = URIs[Math.floor(Math.random()*URIs.length)];
 
-            parentNode.replaceChild(newNode, node);
+                var style = window.getComputedStyle(node.parentNode);
+                var textDecoration = style.getPropertyValue("text-decoration");
+                if(!textDecoration)
+                    textDecoration = "inherit";
+                var font = style.getPropertyValue("font");
+                if(!font)
+                    font = "inherit";
+                var display = style.getPropertyValue("display");
+                if(!display)
+                    display = "inherit";
+
+                newNode.innerHTML = node.data.replace(searchString, "<a style='text-decoration:inherit;font:inherit;display:inherit' href='" +  URI + "'>$1</a>");
+
+                parentNode.replaceChild(newNode, node);
+            }
 
         } else if(node.nodeType == Element.ELEMENT_NODE){
             //  Check this node's child nodes for text nodes to act on
